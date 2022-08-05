8d9a2868f90396c4fb1e3a4a359fa08bd9c2b00b
kely.lopes <caju@MacBook-Pro-de-CAJU.local>
2022-08-04 21:51:27 -0300

    feat: add final styling

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -6,9 +6,11 @@ export function TodoList(){
     return (
         <div className={styles.todoListContent}>
             <div className={styles.todoListTask}>
-                <div className={styles.todoItemList}>
-                  <input type="radio" /> <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
-                </div>
+                <label className={styles.todoItemList}>
+                  <input type="checkbox" />
+                  <span className={styles.checkmark}></span>
+                </label>
+                <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
                 <button type="submit">
                   <img src={iconTrash} alt=""/>
                 </button>
